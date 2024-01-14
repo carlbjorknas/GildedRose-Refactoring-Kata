c435deeb08d521bbef776a627eb2783ae4a176d4
Carl Björknäs <carl.bjorknas@gmail.com>
2024-01-14 07:58:55 +0100

    Justerar Quality i ett steg för "Övrigt".

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -44,13 +44,11 @@ public class GildedRose
                 continue;
             }
             
-            item.Quality--;
             item.SellIn--;
 
-            if (item.SellIn < 0)
-            {
-                item.Quality--;
-            }
+            item.Quality = item.SellIn < 0
+                ? item.Quality - 2
+                : item.Quality - 1;
 
             if (item.Quality < 0)
                 item.Quality = 0;
