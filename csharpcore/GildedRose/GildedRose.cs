29053c048228860aa992342584512ab4221e0017
Carl Björknäs <carl.bjorknas@gmail.com>
2024-01-14 07:30:24 +0100

    Byter for-loop till foreach för läsbarhet.

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -13,64 +13,64 @@ public class GildedRose
 
     public void UpdateQuality()
     {
-        for (var i = 0; i < _items.Count; i++)
+        foreach(var item in _items)
         {
-            if (_items[i].Name == "Sulfuras, Hand of Ragnaros")
+            if (item.Name == "Sulfuras, Hand of Ragnaros")
                 continue;
 
-            if (_items[i].Name == "Aged Brie")
+            if (item.Name == "Aged Brie")
             {
-                _items[i].SellIn = _items[i].SellIn - 1;
-                if (_items[i].Quality < 50)
-                    _items[i].Quality = _items[i].Quality + 1;
+                item.SellIn = item.SellIn - 1;
+                if (item.Quality < 50)
+                    item.Quality = item.Quality + 1;
                 continue;
             }
             
-            if (_items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (_items[i].Quality > 0)
+                if (item.Quality > 0)
                 {
-                    _items[i].Quality = _items[i].Quality - 1;
+                    item.Quality = item.Quality - 1;
                 }
             }
             else
             {
-                if (_items[i].Quality < 50)
+                if (item.Quality < 50)
                 {
-                    _items[i].Quality = _items[i].Quality + 1;
+                    item.Quality = item.Quality + 1;
 
-                    if (_items[i].SellIn < 11)
+                    if (item.SellIn < 11)
                     {
-                        if (_items[i].Quality < 50)
+                        if (item.Quality < 50)
                         {
-                            _items[i].Quality = _items[i].Quality + 1;
+                            item.Quality = item.Quality + 1;
                         }
                     }
 
-                    if (_items[i].SellIn < 6)
+                    if (item.SellIn < 6)
                     {
-                        if (_items[i].Quality < 50)
+                        if (item.Quality < 50)
                         {
-                            _items[i].Quality = _items[i].Quality + 1;
+                            item.Quality = item.Quality + 1;
                         }
                     }                    
                 }
             }
 
-            _items[i].SellIn = _items[i].SellIn - 1;
+            item.SellIn = item.SellIn - 1;
 
-            if (_items[i].SellIn < 0)
+            if (item.SellIn < 0)
             {
-                if (_items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (_items[i].Quality > 0)
+                    if (item.Quality > 0)
                     {
-                        _items[i].Quality = _items[i].Quality - 1;
+                        item.Quality = item.Quality - 1;
                     }
                 }
                 else
                 {
-                    _items[i].Quality = 0;
+                    item.Quality = 0;
                 }
             }
         }
