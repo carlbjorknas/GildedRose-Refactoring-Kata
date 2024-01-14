5ec4dbe2bc3e506ac65fdf56595630c855b973ba
Carl Björknäs <carl.bjorknas@gmail.com>
2024-01-14 07:52:39 +0100

    Byter flera grenar av if till switch för "Backstage passes".

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -30,14 +30,13 @@ public class GildedRose
             {
                 item.SellIn--;
 
-                if (item.SellIn < 0)
-                    item.Quality = 0;
-                else if (item.SellIn < 5)
-                    item.Quality += 2;
-                else if (item.SellIn < 10)
-                    item.Quality += 3;
-                else
-                    item.Quality++;
+                item.Quality = item.SellIn switch
+                {
+                    < 0 => 0,
+                    < 5 => item.Quality + 2,
+                    < 10 => item.Quality + 3,
+                    _ => item.Quality + 1,
+                };
 
                 if (item.Quality > 50)
                     item.Quality = 50;
