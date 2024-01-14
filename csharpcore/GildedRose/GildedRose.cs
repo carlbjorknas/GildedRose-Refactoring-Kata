363f3ee3ca8cea9fc1cedea9e0a1c60db76ec924
Carl Björknäs <carl.bjorknas@gmail.com>
2024-01-14 07:49:27 +0100

    Ökar Quality på "Backstage passes" med rätt värde direkt istället för steeg för steg.

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -29,23 +29,18 @@ public class GildedRose
             if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
                 item.SellIn--;
-                item.Quality++;
-
-                if (item.SellIn < 10)
-                {
-                    item.Quality++;
-                }
-
-                if (item.SellIn < 5)
-                {
-                    item.Quality++;
-                }
-
-                if (item.Quality > 50)
-                    item.Quality = 50;                
 
                 if (item.SellIn < 0)
                     item.Quality = 0;
+                else if (item.SellIn < 5)
+                    item.Quality += 2;
+                else if (item.SellIn < 10)
+                    item.Quality += 3;
+                else
+                    item.Quality++;
+
+                if (item.Quality > 50)
+                    item.Quality = 50;
 
                 continue;
             }
